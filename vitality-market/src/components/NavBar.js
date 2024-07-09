91436494cb23e9cc362128a6213f72247f38f0d7
Tyeisha Stroud <tyeishastroud@gmail.com>
2024-07-08 23:00:36 -0400

    The completed NavBar

--- vitality-market/src/components/NavBar.js
+++ vitality-market/src/components/NavBar.js
@@ -1,22 +1,63 @@
-import {useRef} from "react";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/NavBar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChessQueen, faComment, faCircleUser} from '@fortawesome/free-regular-svg-icons'
+import { faPencil, faSpa, faBasketShopping, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
 
 
 function NavBar(){
-    return (
-        <header>
-            <nav>
-                <a href="/#">About Us</a>
-                <a href="/#">Testimonials</a>
-                <a href="/#">Contact</a>
-            </nav>
-            <h1>VITALITY MARKETPLACE</h1>
-            <nav>
-            <a href="/#">Search</a>
-            <a href="/#">Cart</a>
-            <a href="/#">Account</a>
-            </nav>
-        </header>
-    );
+     return(
+     <>     
+        <section className='iconBar'>
+            <Link to="/vitality-market/src/page/AboutUs.js" className='aboutUs'>
+                <FontAwesomeIcon icon={faChessQueen} className='icon' />
+                <br/>
+                <p>About Us</p>
+           </Link>
+           
+           <Link to="/vitality-market/src/page/Testimonial.js" className='test'>
+                <FontAwesomeIcon icon={faPencil} className='icon'/>
+                <br/>
+                <p>Testimonial</p>
+           </Link>
+
+           <Link to="/vitality-market/src/page/Contact.js" className='contact'>
+                <FontAwesomeIcon icon={faComment} className='icon'/>
+                <br/>
+                <p>Contact Us</p>
+           </Link>
+
+           <h1 className='title'>VITALITY <FontAwesomeIcon icon={faSpa}/> MARKETPLACE</h1>
+
+           <Link to="" className='search'>
+                <FontAwesomeIcon icon={faMagnifyingGlass} className='icon'/>
+                <br/>
+                <p>Search</p>
+           </Link>
+
+           <Link to="/vitality-market/src/page/Cart.js" className='cart'>
+                <FontAwesomeIcon icon={faBasketShopping} className='icon'/>
+                <br/>
+                <p>Cart</p>
+           </Link>
+
+           <Link to="/vitality-market/src/page/Account.js" className='account'>
+                <FontAwesomeIcon icon={faCircleUser} className='icon'/>
+                <br/>
+                <p>Account</p>
+           </Link>      
+        </section>
+
+        <div className='subContainer'>
+          <h2 className='subtitle'>SHOP</h2>
+          <h2 className='subtitle'>NEW ARRIVALS</h2>
+          <h2 className='subtitle'>*SPECIALS*</h2>
+          <h2 className='subtitle'>BESTSELLERS</h2>
+        </div>
+        </>
+     )
 }
 
 export default NavBar;
+
